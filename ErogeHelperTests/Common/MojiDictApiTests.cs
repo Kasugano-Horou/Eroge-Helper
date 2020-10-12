f33194b34148402e4a4e948d00b96365fd0b98b0
k1mlka <k1mlka@outlook.com>
2020-10-13 06:51:52 +0800

    MojiAPI with MSTest

---
+++ ErogeHelperTests/Common/MojiDictApiTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ErogeHelper.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ErogeHelper.Common.Tests
+{
+    [TestClass()]
+    public class MojiDictApiTests
+    {
+        [TestMethod()]
+        public async Task MojiDictApiTest()
+        {
+            var query = "私";
+            var result = await new MojiDictApi().RequestAsync(query);
+            Assert.AreEqual(query, result.result.word.spell);
+        }
+    }
+}
